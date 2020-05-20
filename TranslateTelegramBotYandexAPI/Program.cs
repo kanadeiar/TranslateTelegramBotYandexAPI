0a561a9ff728c7b8a2eeecb40fb082089332d120
Sergei Kamianetskii <i@ksergey.ru>
2020-05-20 21:04:41 +0300

    translate api ya

--- TranslateTelegramBotYandexAPI/Program.cs
+++ TranslateTelegramBotYandexAPI/Program.cs
@@ -1,17 +1,47 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using Telegram.Bot;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
 
 namespace TranslateTelegramBotYandexAPI
 {
+    // https://yandex.ru/dev/
+    // https://core.telegram.org/api
+    // https://yandex.ru/dev/translate/
     class Program
     {
+        static string apiKey = " ";
+
+        private static string GetEngText(string text)
+        {
+            string url = $"https://translate.yandex.net/api/v1.5/tr.json/translate?key={apiKey}&text={text}&lang=ru-en&format=plain";
+            var json = new WebClient() { Encoding = Encoding.UTF8 }.DownloadString(url);
+            dynamic jObject = JObject.Parse(json);
+            return jObject.text[0];
+        }
+
         static void Main(string[] args)
         {
+            TelegramBotClient bot = new TelegramBotClient(File.ReadAllText(@"D:\Work\token.txt"));
 
+            bot.OnMessage += (s, arg) =>
+            {
+                Console.WriteLine($"{arg.Message.Chat.FirstName}: {arg.Message.Text}");
+                bot.SendTextMessageAsync(arg.Message.Chat.Id, $"Вы прислали: {GetEngText(arg.Message.Text)}");
+            };
 
+            bot.StartReceiving();
+
+            Console.ReadKey();
         }
+
+      
     }
 }
